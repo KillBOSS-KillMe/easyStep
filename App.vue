e77079a27b8d99272accf229af870f8bc6b87f1c
luow <429175116@qq.com>
2020-04-16 10:56:48 +0800

    创建活动页，个人中心页，导入导航图片，导入icon库，初始化css

--- App.vue
+++ App.vue
@@ -1,3 +1,55 @@
+<!-- 阴影边框
+box-shadow: #f1fefb 1rpx 1rpx 10rpx 5rpx; -->
+
+<!-- 
+flexflexflexflexflexflexflexflex
+display: flex;
+align-items: center;
+justify-content: space-between; -->
+
+<!-- 竖向
+display: flex;
+align-items: center;
+justify-content: space-between;
+flex-direction: column; -->
+
+<!-- 换行
+display: flex;
+align-items: center;
+justify-content: space-between;
+flex-direction: row;
+flex-wrap: wrap; -->
+
+<!-- 
+换行隐藏
+overflow : hidden;
+text-overflow: ellipsis;
+display: -webkit-box;
+-webkit-line-clamp: 2;
+-webkit-box-orient: vertical; -->
+
+<!-- 
+居右
+flex-direction:row;
+justify-content:flex-end
+vertical-align:middle; -->
+
+<!-- 
+自动换行
+word-break:break-all; -->
+
+<!-- 单行文本溢出
+overflow:hidden;
+text-overflow:ellipsis;
+white-space:nowrap; -->
+
+<!-- 多行文本溢出 -->
+<!-- -webkit-line-clamp: 2;
+display: -webkit-box;
+-webkit-box-orient: vertical;
+overflow: hidden;
+text-overflow: ellipsis; -->
+
 <script>
 	export default {
 		onLaunch: function() {
@@ -13,5 +65,75 @@
 </script>
 
 <style>
-	/*每个页面公共css */
+	.page {
+	  background-color: #fafafa;
+	  width: 100vw;
+	  min-height: 100vh;
+	  height: auto;
+	}
+	/* #ifdef H5 */
+	uni-checkbox .uni-checkbox-input {
+	  border-radius: 50% !important;
+	  color: #ffffff !important;
+	}
+	
+	uni-checkbox .uni-checkbox-input.uni-checkbox-input-checked {
+	  border: none !important;
+	  background: #9FD8F5;
+		border-color: #9FD8F5;
+	}
+	
+	uni-checkbox .uni-checkbox-input.uni-checkbox-input-checked::before {
+		width: 20rpx;
+		height: 20rpx;
+		line-height: 20rpx;
+		text-align: center;
+		font-size: 18rpx;
+		color: #fff;
+		background: transparent;
+		transform: translate(-70%, -50%) scale(1);
+		-webkit-transform: translate(-70%, -50%) scale(1);
+	}
+	
+	/* #endif */
+	/* 微信样式 */
+	/* #ifdef APP-PLUS ||MP-WEIXIN */
+	checkbox .wx-checkbox-input {
+	  border-radius: 50% !important;
+	  color: #ffffff !important;
+	}
+	
+	checkbox .wx-checkbox-input.wx-checkbox-input-checked {
+	  color: #fff;
+	  background: #9FD8F5;
+	}
+	
+	.wx-checkbox-input.wx-checkbox-input-checked {
+	  border: none !important;
+	}
+	
+	button {
+	  position: relative;
+	  display: block;
+	  padding: 0;
+	  margin: 0;
+	  line-height: auto;
+	  box-sizing: border-box;
+	  text-align: center;
+	  text-decoration: none;
+	  -webkit-tap-highlight-color: transparent;
+	  overflow: hidden;
+	  color: #000000;
+	  background: none;
+	}
+	button::after {
+	  border: none;
+	}
+	.iconfont {
+	  display: flex;
+	  align-items: center;
+	  justify-content: space-between;
+	}
+	
+	/* #endif */
 </style>
